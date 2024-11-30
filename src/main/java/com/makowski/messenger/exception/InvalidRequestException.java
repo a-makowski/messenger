bb34e2b4f75d51e995944d9305586562fa66c3e2
Anna Makowski <a.makowski723@gmail.com>
2024-12-01 00:05:03 +0100

    Fix typo

--- src/main/java/com/makowski/messenger/exception/InvalidRequestException.java
+++ src/main/java/com/makowski/messenger/exception/InvalidRequestException.java
@@ -1,9 +1,16 @@
 package com.makowski.messenger.exception;
 
 public class InvalidRequestException extends RuntimeException {
+
     public InvalidRequestException() {
         super("Invalid Request");
     }
-    public InvalidRequestException(String username) { super("Username " + username + " is already taken"); }
-    public InvalidRequestException(int characters) { super("This message is " + characters + " charakters too long"); }
+
+    public InvalidRequestException(String username) {
+        super("Username " + username + " is already taken");
+    }
+
+    public InvalidRequestException(int characters) {
+        super("This message is " + characters + " characters too long");
+    }
 }
